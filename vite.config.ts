4b61f5f5ff41965904128c8ccd21c42e24983e4f
Lucas Vilas Boas <lucas.vilasboas@dtidigital.com.br>
2024-07-18 14:50:15 -0300

    vitest proper setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const viteConfig = defineViteConfig({
     plugins: [react()],
 });
+
+const vitestConfig = defineVitestConfig({
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/tests/setup.ts',
+    },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
